dbcfacaca909429fd8e95c8bdb9198323bf3a2f9
pradeepbgs <kumarpradeepbgs@gmail.com>
2024-11-18 20:07:30 +0530

    improved design and made header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
-import { Stack } from "expo-router";
-import { StyleSheet, View, StatusBar } from "react-native";
+import { Stack, Tabs } from "expo-router";
+import { StyleSheet, View, StatusBar, Image } from "react-native";
 
 export default function RootLayout() {
   return (
@@ -12,9 +12,22 @@ export default function RootLayout() {
           headerShown: false,                 
         }}
       >
-        <Stack.Screen name="(home)" options={{ headerShown: false }} />
-        <Stack.Screen name="(profile)" options={{headerShown:false}}/>
-        <Stack.Screen name="(video)" options={{headerShown:false}} />
+        <Stack.Screen 
+        name="(home)" 
+        options={{ 
+          headerShown: false,
+        }} 
+        />
+
+        <Stack.Screen 
+        name="(profile)" 
+        options={{headerShown:false}}
+        />
+
+        <Stack.Screen 
+        name="(video)" 
+        options={{headerShown:false}} 
+        />
       </Stack>
     </View>
   );
@@ -24,5 +37,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "black",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    paddingHorizontal: 10,
+  },
+  logo: {
+    width: 40,
+    height: 40,
+    resizeMode: "contain",
   },
 });
